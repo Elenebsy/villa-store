8ac43154feafff473a64f95d01e8faf9eba46304
bgbos7077 <bgbos7077@gmail.com>
2023-12-06 22:11:28 +0200

    Some elements like that: Shopcart icon,Categories

---
+++ frontend/my-app/src/Components/Categories/Houses.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import propertyImage from "../Assets/property/property1.png";
+
+const Houses = () => {
+  return (
+    <div>
+      <img src={propertyImage} alt="Property" />
+    </div>
+  );
+}
+
+export default Houses;
