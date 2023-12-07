aa9283e38f35bb36dfe85e508ebe59d359b55d32
kamal <kamalhagag021@gmail.com>
2023-12-07 16:44:25 +0200

    slider addtion

--- frontend/my-app/src/Components/Pages/Home.js
+++ frontend/my-app/src/Components/Pages/Home.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import Slider from './slider/Slider';
 export default class Home extends Component {
   render() {
     return (
       <>
+      <Slider/>
       <dev>he</dev>
       </>
     )
