ca75cec5c1182753e31758aecf50253ad57d0547
Kareem-Koga <141167691+Kareem-Koga@users.noreply.github.com>
2023-12-08 09:31:32 +0200

    deleting some un used

--- frontend/my-app/src/Components/Pages/Home.js
+++ frontend/my-app/src/Components/Pages/Home.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
-import Slider from '../slider/Slider.jsx';
+
 export default class Home extends Component {
   render() {
     return (
       <>
-      <Slider/>
       <dev>he</dev>
       </>
     )
