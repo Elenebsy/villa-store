25b6cb6f2bb5e8314580d3165ed19385d478b224
MohamedKhaled5011 <mohamedkhaled0453704@gmail.com>
2023-12-08 02:49:47 +0200

    Search Bar
    Search Bar

--- frontend/my-app/src/Components/Pages/Home.js
+++ frontend/my-app/src/Components/Pages/Home.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
-
+import Search from '../Search-Bar/Search'
 export default class Home extends Component {
   render() {
     return (
-      <div>home</div>
+      <>
+      <Search />
+      </>
+
+    
+
     )
   }
 }
