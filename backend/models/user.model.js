6e068a841d9314469f2bc2362426623e86383305
Elenebsy <bdalkhalqbdalrhym06@gmail.com>
2023-12-09 12:42:16 +0200

    zdsks

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,19 +3,16 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
   {
-    user_id: { type: String, unique: true, required: false },
+    // user_id: { type: String, unique: false, required: false },
     fullName: { type: String, required: true },
     email: { type: String, unique: true, required: true },
-    address: { type: String,  required: true },
+    address: { type: String, required: false },
     password: { type: String, required: true },
     phone: { type: String, required: true },
     image: { type: String, required: false },
-    
   },
   {
     timestamps: true,
   }
 );
 module.exports = mongoose.model("User", userSchema);
-
-
